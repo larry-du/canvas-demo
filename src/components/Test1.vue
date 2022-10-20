1cafd723ddcb98b2b4c06a58ab2a893fd8c2b462
larrydu <larrydu@msi.com>
2022-10-20 17:04:55 +0800

    test component is

---
+++ src/components/Test1.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>11111111111111111</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Test2.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>22222222222</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
